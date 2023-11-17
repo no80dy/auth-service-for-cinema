edad8979abc6f85a0bfb6463ea779f66716ef9d8
Ruslan Rakhmatullin <RakhmatullinRR@tatneft.tatar>
2023-11-17 23:23:52 +0300

    pep8 + при регистрации обработка существующего логина

--- src/api/v1/users.py
+++ src/api/v1/users.py
@@ -1,28 +1,34 @@
 from http import HTTPStatus
 
 from fastapi import APIRouter, Depends
+from fastapi import HTTPException
 from fastapi.encoders import jsonable_encoder
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
-
-from schemas.entity import UserInDB, UserCreate
 
 from db.postgres import get_session
 from models.entity import User
-
+from schemas.entity import UserInDB, UserCreate
 
 router = APIRouter()
 
 
 @router.post(
-	'/signup',
-	response_model=UserInDB,
-	status_code=HTTPStatus.CREATED
+    '/signup',
+    response_model=UserInDB,
+    status_code=HTTPStatus.CREATED
 )
 async def create_user(
-	user_create: UserCreate,
-	db: AsyncSession = Depends(get_session)
+        user_create: UserCreate,
+        db: AsyncSession = Depends(get_session)
 ) -> UserInDB:
     user_dto = jsonable_encoder(user_create)
+
+    result = await db.execute(select(User).where(User.login == user_dto.get('login')))
+    user = result.scalars().first()
+    if user:
+        raise HTTPException(status_code=401, detail="Bad username or password")
+
     user = User(**user_dto)
     db.add(user)
     await db.commit()
