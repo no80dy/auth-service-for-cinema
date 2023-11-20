7d8922859fc4a77c98317d442a494d5be4629dbc
Ruslan Rakhmatullin <RakhmatullinRR@tatneft.tatar>
2023-11-20 20:26:05 +0300

    реализация смены пароля клиента

--- src/schemas/entity.py
+++ src/schemas/entity.py
@@ -18,3 +18,15 @@ class UserInDB(BaseModel):
 
     class Config:
         from_attributes = True
+
+
+class UserChangePassword(BaseModel):
+    username: str = Field(..., max_length=255)
+    password: str = Field(..., min_length=8, max_length=255)
+    repeaded_old_password: str = Field(..., min_length=8, max_length=255)
+
+    new_password: str = Field(..., min_length=8, max_length=255)
+
+
+class UserResponseUsername(BaseModel):
+    username: str = Field(..., max_length=255)
